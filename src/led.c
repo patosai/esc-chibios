b8a5e37ab5ac9473eae4717be9c48981b8cb07b5
Patrick Tsai <patosai@users.noreply.github.com>
2020-06-29 14:21:14 -0700

    Fix frequency calculations

--- src/led.c
+++ src/led.c
@@ -4,90 +4,46 @@
 #include "led.h"
 
 void led_1_turn_on(void) {
-#ifdef DISCOVERY
-    palSetPadMode(GPIOD, 12, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPad(GPIOD, 12);
-#else
-    palSetPadMode(GPIOC, 8, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPad(GPIOC, 8);
-#endif
+  palSetPadMode(GPIOC, 8, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPad(GPIOC, 8);
 }
 
 void led_1_turn_off(void) {
-#ifdef DISCOVERY
-    palClearPad(GPIOD, 12);
-#else
-    palClearPad(GPIOC, 8);
-#endif
+  palClearPad(GPIOC, 8);
 }
 
 void led_2_turn_on(void) {
-#ifdef DISCOVERY
-    palSetPadMode(GPIOD, 13, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPad(GPIOD, 13);
-#else
-    palSetPadMode(GPIOC, 9, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPad(GPIOC, 9);
-#endif
+  palSetPadMode(GPIOC, 9, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPad(GPIOC, 9);
 }
 
 void led_2_turn_off(void) {
-#ifdef DISCOVERY
-    palClearPad(GPIOD, 13);
-#else
-    palClearPad(GPIOC, 9);
-#endif
+  palClearPad(GPIOC, 9);
 }
 
 void led_3_turn_on(void) {
-#ifdef DISCOVERY
-    palSetPadMode(GPIOD, 14, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPad(GPIOD, 14);
-#else
-    palSetPadMode(GPIOC, 10, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPad(GPIOC, 10);
-#endif
+  palSetPadMode(GPIOC, 10, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPad(GPIOC, 10);
 }
 
 void led_3_turn_off(void) {
-#ifdef DISCOVERY
-    palClearPad(GPIOD, 14);
-#else
-    palClearPad(GPIOC, 10);
-#endif
+  palClearPad(GPIOC, 10);
 }
 
 void led_4_turn_on(void) {
-#ifdef DISCOVERY
-    palSetPadMode(GPIOD, 15, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPad(GPIOD, 15);
-#else
-    palSetPadMode(GPIOE, 0, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPad(GPIOE, 0);
-#endif
+  palSetPadMode(GPIOE, 0, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPad(GPIOE, 0);
 }
 
 void led_4_turn_off(void) {
-#ifdef DISCOVERY
-    palClearPad(GPIOD, 15);
-#else
-    palClearPad(GPIOE, 0);
-#endif
+  palClearPad(GPIOE, 0);
 }
 
 void led_5_turn_on(void) {
-#ifdef DISCOVERY
-    // noop
-#else
-    palSetPadMode(GPIOE, 1, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPad(GPIOE, 1);
-#endif
+  palSetPadMode(GPIOE, 1, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPad(GPIOE, 1);
 }
 
 void led_5_turn_off(void) {
-#ifdef DISCOVERY
-    // noop
-#else
-    palClearPad(GPIOE, 1);
-#endif
+  palClearPad(GPIOE, 1);
 }
